e17572c21d036ec54790a5656120c3f82c0eb911
auxiliarymaxima <93989258+auxiliarymaxima@users.noreply.github.com>
2025-04-18 00:54:05 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker installed');
-});
-
-self.addEventListener('fetch', function(event) {
-  // You can add caching logic here if needed
-});
