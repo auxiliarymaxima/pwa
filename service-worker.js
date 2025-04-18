0ef74ff0bcdb811758d9eeb7eea866b53c9d9e0d
auxiliarymaxima <93989258+auxiliarymaxima@users.noreply.github.com>
2025-04-18 07:18:14 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-
 self.addEventListener('install', event => {
-  console.log('Service Worker installing.');
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  // Send a message back to the page
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => {
+      client.postMessage("Service worker installed!");
+    });
+  });
 });
